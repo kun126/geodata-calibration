9a514e810755a9706184ea5b9c6f6b855b583603
Kun Chen <kun.chen@student.kuleuven.be>
2024-08-12 22:13:45 +0200

    test linting

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,11 @@
-from geodata_calibration.utils import reproj_crop_for_vector, reproj_crop_for_raster
+from geodata_calibration.utils import reproj_crop_for_raster, reproj_crop_for_vector
 
 
 def test_reproj_crop_for_vector():
     """Tests for reproj_crop_for_vector()."""
+    pass
+
 
 def test_reproj_crop_for_raster():
     """Tests for reproj_crop_for_raster()."""
+    pass
